08aae3735e69c0e2d31e305010c63b8f9858f7d5
iagomachado <iago.machado000@gmail.com>
2023-05-29 09:46:58 -0300

    Adicionando bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.site')
+
+@section('content')
+    <h1>Olá mundo!</h1>
+@endsection
---
+++ resources/views/layout/site.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hamburgueria do Centro</title>
+
+    <!-- Carregando assets -->
+    @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
